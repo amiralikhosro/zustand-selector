716bb94fc277c1cd891fbdb541569fbb175ae0e2
ali.khosro72@gmail.com <ali.khosro72@gmail.com>
2023-01-12 17:33:01 +0330

    feat: initial commit ad structor project and store

---
+++ zustand-selectors/src/App.tsx
@@ -0,0 +1,78 @@
+import { Box, Button, Text } from "@mantine/core";
+
+import { useStore } from "./store";
+
+const DoNothingButton = () => {
+  const doNothing = useStore((state) => state.doNothing);
+  return (
+    <Button size="xl" onClick={doNothing}>
+      Do Nothing
+    </Button>
+  );
+};
+
+const AddToFirstButton = () => {
+  const addToFirst = useStore((state) => state.addToFirst);
+  return (
+    <Button size="xl" onClick={addToFirst}>
+      Add To First
+    </Button>
+  );
+};
+
+const FirstValue = () => {
+  const { firstNumber } = useStore();
+  return (
+    <Text size="xl" p={5}>
+      First Value: {firstNumber}
+    </Text>
+  );
+};
+
+const AddToSecondButton = () => {
+  const addToSecond = useStore((state) => state.addToSecond);
+  return (
+    <Button size="xl" onClick={addToSecond}>
+      Add To First
+    </Button>
+  );
+};
+
+const SecondValue = () => {
+  const secondNumber = useStore((state) => state.secondNumber);
+  return (
+    <Text size="xl" p={5}>
+      Second Value: {secondNumber}
+    </Text>
+  );
+};
+
+const NumbersValue = () => {
+  const numbers = useStore((state) => state.numbers);
+  return (
+    <Text size="xl" p={5}>
+      Numbers: {numbers.join(", ")}
+    </Text>
+  );
+};
+
+function App() {
+  return (
+    <Box p={10}>
+      <Box sx={{ display: "flex" }}>
+        <DoNothingButton />
+      </Box>
+      <Box mt={10} sx={{ display: "flex" }}>
+        <AddToFirstButton />
+        <FirstValue />
+      </Box>
+      <Box mt={10} sx={{ display: "flex" }}>
+        <AddToSecondButton />
+        <SecondValue />
+      </Box>
+      <NumbersValue />
+    </Box>
+  );
+}
+
+export default App;
